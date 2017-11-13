e690b2ff603423025cac303ff143b2faacd27177
Jose <j.albertorodriguez89@gmail.com>
2017-11-13 21:17:17 +1030

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return 'HELLO RASPBERRY'
+
+if __name__ == '__main__':
+    app.run()
