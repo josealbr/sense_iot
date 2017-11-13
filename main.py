4e6f5f970e6ea92a21e44ade9f0127d9e3dc9e09
Jose <j.albertorodriguez89@gmail.com>
2017-11-13 21:40:23 +1030

    1

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
+from sense_hat import SenseHat
 
 app = Flask(__name__)
 
+sense = SenseHat()
+
 @app.route("/")
 def main_page():
+    sense.show_letter('H')
     return 'HELLO RASPBERRY'
+
+@app.route("/temp")
+def temp():
+    temp = sense.get_temperature() - 6
+    return jsonify(t)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
